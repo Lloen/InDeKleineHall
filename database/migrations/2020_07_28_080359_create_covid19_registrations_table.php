e1222b901b444d8cd3f82b63696d4a52cb9a2e1e
Lloen <lloen@ymail.com>
2020-07-28 10:25:03 +0200

    Added covid19 registration database migration

---
+++ database/migrations/2020_07_28_080359_create_covid19_registrations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCovid19RegistrationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('covid19_registrations', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->integer('number_of_people');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('covid19_registrations');
+    }
+}
