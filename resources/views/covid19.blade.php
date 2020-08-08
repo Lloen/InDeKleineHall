06733d42720204a3bb75655406b02bf5af58c282
Lloen <lloen@ymail.com>
2020-08-08 11:18:50 +0200

    Added check from svg icon

--- resources/views/covid19.blade.php
+++ resources/views/covid19.blade.php
@@ -87,7 +87,11 @@
                 <input type="submit" class="btn btn-primary btn-lg btn-block" value="Verzenden" />
             </form>
             @else
-            <h1 class="card-text text-center">✔️</h1>
+            <div class="text-center mb-3">
+                <svg width="5em" height="5em" viewBox="0 0 16 16" class="bi bi-check-square-fill" fill="#008000" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                </svg>
+            </div>
             <h2 class="card-text text-center"> Geregistreerd voor vandaag, laat dit aan de ober zien. </h2>
             <h2 class="card-text text-center"> Stay safe! </h2>
             <h5 class="card-text text-center"> With ❤️ In De Kleine Hal </h5>
