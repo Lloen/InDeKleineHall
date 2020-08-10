269b9a926dcb4821efa2ddf6dd84ebb42aff76d8
Lloen <lloen@ymail.com>
2020-08-10 16:54:17 +0200

    Added Logged In users menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,27 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('menu') }}">{{ __('🍺 Menu') }}</a>
                         </li>
+                        @if(Auth::check())
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('covid19Registraties') }}">
+                                    {{ __('Covid-19 registraties') }}
+                                </a>
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endif
                     </ul>
                 </div>
             </div>
