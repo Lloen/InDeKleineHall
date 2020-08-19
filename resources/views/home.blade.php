9d1308e9b14e8040c9d1ba302c067e83e3930ff0
Lloen <lloen@ymail.com>
2020-08-19 15:56:05 +0200

    Added a small button menu for the dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,21 @@
-@extends('layouts.app')
+@extends('layouts.appDashboard')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <a type="button" class="btn btn-outline-dark btn-block" href="{{ route('covid19Registraties') }}">Covid-19 registraties</a>
+                    <a type="button" class="btn btn-outline-dark btn-block" href="{{ route('covid19Registraties') }}">Menu</a>
+                    <a type="button" class="btn btn-danger btn-block" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        Uitloggen
+                    </a>
 
-                    {{ __('You are logged in!') }}
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </div>
             </div>
         </div>
