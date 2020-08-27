f1ec1861f571cc5a6ee0a76815f31cdd78173cee
Lloen <lloen@ymail.com>
2020-08-27 12:25:13 +0200

    Added NL meta language

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,10 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="nl" xml:lang="nl">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="The oldest pub in Hasselt, Belgium. ET 17th century">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
