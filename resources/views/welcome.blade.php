315be6c4aa392d43158f2e9c1b604dd72195dbfd
Lloen <lloen@ymail.com>
2020-08-28 11:38:07 +0200

    Changed the root page from the default to a new one

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,104 +1,66 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
+<style>
+    body,
+    html {
+        height: 100%;
+    }
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    .blur {
+        height: 100%;
+        background: rgba(0, 0, 0, 0.4);
+        backdrop-filter: blur(15px);
+    }
 
-    <title>Laravel</title>
+    .bgimg-1 {
+        background-image: url("{{ asset('img/Welcome.InDeKleineHal.jpg') }}");
+        background-size: cover;
+    }
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    .bgimg-2 {
+        background-image: url("{{ asset('img/OldPicture.InDeKleineHal.jpg') }}");
+        background-size: none;
+    }
 
-    <!-- Styles -->
-    <style>
-        html,
-        body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
+    .bgimg-1,
+    .bgimg-2,
+    .bgimg-3 {
+        min-height: 100%;
+        max-width: 100%;
+        height: 100%;
+        position: relative;
 
-        .full-height {
-            height: 100vh;
-        }
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: fill;
+    }
+</style>
 
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links>a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-            <a href="{{ url('/home') }}">Home</a>
-            @else
-            <a href="{{ route('login') }}">Login</a>
-
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}">Register</a>
-            @endif
-            @endauth
-        </div>
-        @endif
-
-        <div class="content">
-            <div class="title m-b-md">
-                Laravel
-            </div>
-
-            <div class="links">
-                <a href="https://laravel.com/docs">Docs</a>
-                <a href="https://laracasts.com">Laracasts</a>
-                <a href="https://laravel-news.com">News</a>
-                <a href="https://blog.laravel.com">Blog</a>
-                <a href="https://nova.laravel.com">Nova</a>
-                <a href="https://forge.laravel.com">Forge</a>
-                <a href="https://vapor.laravel.com">Vapor</a>
-                <a href="https://github.com/laravel/laravel">GitHub</a>
-            </div>
-        </div>
+@section('content')
+<div class="bgimg-1">
+    <div class="col-sm-12 d-flex align-items-center justify-content-center blur">
+        <img class="" src="{{ asset('img/InDeKleineHalWhiteLogo.png') }}" width="176" height="247">
     </div>
-</body>
+</div>
 
-</html>
+<div class="card align-items-center bgimg-Divider">
+    <div class="card-body my-3">
+        <h3 class="card-title text-center">The oldest pub in Hasselt</h3>
+        <h6 class="card-subtitle mb-2 text-muted text-center">Since 1836</h6>
+        <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+</div>
+
+<div class="bgimg-2">
+</div>
+
+<div class="card align-items-center bgimg-Divider">
+    <div class="card-body my-3">
+        <h3 class="card-title text-center">Our drinks</h3>
+        <h6 class="card-subtitle mb-2 text-muted text-center">We have a great variety of alcholic and non-alcholoic drinks</h6>
+        <a class="btn btn-primary btn-lg text-center btn-block" href="{{ route('menu') }}">{{ __('🍺 Check out our Menu 🍺') }}</a>
+    </div>
+</div>
+
+<div class="parallax"></div>
+@endsection
